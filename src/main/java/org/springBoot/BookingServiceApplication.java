91b7cb14df7d2bce7fd0082180571aad942aeaa2
mayahNeko <mayahibela@gmail.com>
2026-03-11 14:50:18 +0300

    feat: change BookingService name

--- src/main/java/org/springBoot/BookingServiceApplication.java
+++ src/main/java/org/springBoot/BookingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionSpringBootFastApiApplication {
+public class BookingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntroductionSpringBootFastApiApplication.class, args);
+        SpringApplication.run(BookingServiceApplication.class, args);
     }
 
 }
