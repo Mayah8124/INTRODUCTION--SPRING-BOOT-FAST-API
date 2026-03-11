b31c375e2a261361e5dedf567ef99d77817ba9c4
mayahNeko <mayahibela@gmail.com>
2026-03-11 13:14:06 +0300

    Initialize project

---
+++ src/main/java/org/springBoot/IntroductionSpringBootFastApiApplication.java
@@ -0,0 +1,13 @@
+package org.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringBootFastApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionSpringBootFastApiApplication.class, args);
+    }
+
+}
